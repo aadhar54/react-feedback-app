2f939e522517075c6199f1f15c293fd54fc3d73a
Himanshu Kumar <himanshukumar802123@gmail.com>
2024-09-01 15:31:10 +0530

    update router

--- src/App.js
+++ src/App.js
@@ -1,38 +1,42 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import Header from './components/Header'
-import FeedbackList from './components/FeedbackList'
-import FeedbackStats from './components/FeedbackStats'
-import FeedbackForm from './components/FeedbackForm'
-import AboutIconLink from './components/AboutIconLink'
-import AboutPage from './pages/AboutPage'
-import { FeedbackProvider } from './context/FeedbackContext'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Header from './components/Header';
+import FeedbackList from './components/FeedbackList';
+import FeedbackStats from './components/FeedbackStats';
+import FeedbackForm from './components/FeedbackForm';
+import AboutIconLink from './components/AboutIconLink';
+import AboutPage from './pages/AboutPage';
+import { FeedbackProvider } from './context/FeedbackContext';
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: (
+        <>
+          <FeedbackForm />
+          <FeedbackStats />
+          <FeedbackList />
+        </>
+      )
+    },
+    {
+      path: '/about',
+      element: <AboutPage />
+    }
+  ]);
+
   return (
+    
     <FeedbackProvider>
-      <Router>
-        <Header />
-        <div className='container'>
-          <Routes>
-            <Route
-              path='/'
-              element={
-                <>
-                  <FeedbackForm />
-                  <FeedbackStats />
-                  <FeedbackList />
-                </>
-              }
-            ></Route>
-
-            <Route path='/about' element={<AboutPage />} />
-          </Routes>
-
-          <AboutIconLink />
-        </div>
-      </Router>
+      <div className='container'>
+      <RouterProvider router={router}> 
+      <Header />
+      <AboutIconLink />
+      </RouterProvider>
+      </div>
     </FeedbackProvider>
-  )
+    
+  );
 }
 
-export default App
+export default App;
